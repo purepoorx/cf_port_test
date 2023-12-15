1bab39387a629eb01dc7db0aa98c5ba330460f24
yin2na <yin2na@gmail.com>
2023-12-15 13:22:37 +0800

    first commit

--- main.go
+++ main.go
@@ -6,37 +6,34 @@ import (
 	"sync"
 )
 
-var mux = http.NewServeMux()
 var mu sync.Mutex
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func handler(w http.ResponseWriter, r *http.Request, port string) {
 	mu.Lock()
 	defer mu.Unlock()
 
-	// 获取请求的端口号
-	port := r.URL.Port()
-
-	// 如果端口号为空，默认使用80
-	if port == "" {
-		port = "80"
-	}
-
 	// 构造响应消息
-	//message := fmt.Sprintf("当前请求的端口是：%s", port)
+	message := fmt.Sprintf("当前请求的端口是：%s", port)
 
 	// 将消息写入响应
-	w.Write([]byte(port))
+	w.Write([]byte(message))
 }
 
 func main() {
+	// 创建全局的http.ServeMux
+	mux := http.NewServeMux()
+
 	// 遍历指定的端口列表
 	ports := []string{"80", "8080", "8880", "2052", "2082", "2086", "2095", "443", "2053", "2083", "2087", "2096", "8443"}
 
+	// 注册路由处理函数
 	for _, port := range ports {
-		go func(p string) {
-			// 设置路由处理函数
-			mux.HandleFunc("/", handler)
+		p := port // 为了避免闭包中的迭代变量问题
+		mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			handler(w, r, p)
+		})
 
+		go func(p string) {
 			// 启动Web服务监听指定端口
 			err := http.ListenAndServe(":"+p, mux)
 			if err != nil {
