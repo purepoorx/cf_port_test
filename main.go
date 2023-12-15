d849ede1a838c04db2dcdd1cc83fe62f8d867d83
yin2na <yin2na@gmail.com>
2023-12-15 15:12:59 +0800

    first commit

--- main.go
+++ main.go
@@ -3,45 +3,40 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"sync"
 )
 
-var mu sync.Mutex
+func main() {
+	// 设置路由规则
+	http.HandleFunc("/", dynamicHandler)
 
-func handler(w http.ResponseWriter, r *http.Request, port string) {
-	mu.Lock()
-	defer mu.Unlock()
-
-	// 构造响应消息
-	message := fmt.Sprintf("当前请求的端口是：%s", port)
-
-	// 将消息写入响应
-	w.Write([]byte(message))
+	// 启动Web服务，监听8888端口
+	err := http.ListenAndServe(":8888", nil)
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+	}
 }
 
-func main() {
-	// 遍历指定的端口列表
-	ports := []string{"80", "8080", "8880", "2052", "2082", "2086", "2095", "443", "2053", "2083", "2087", "2096", "8443"}
+// 处理请求的函数
+func dynamicHandler(w http.ResponseWriter, r *http.Request) {
+	// 获取请求路径
+	path := r.URL.Path
 
-	// 启动一个 goroutine 处理一个端口
-	for _, port := range ports {
-		go func(p string) {
-			// 创建每个端口的 ServeMux
-			mux := http.NewServeMux()
+	//// 根据不同的路径返回不同的响应
+	//switch path {
+	//case "/hello":
+	//	fmt.Fprint(w, "hello")
+	//case "/welcome":
+	//	fmt.Fprint(w, "welcome")
+	//default:
+	//	fmt.Fprint(w, "unknown path")
+	//}
 
-			// 注册路由处理函数
-			mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-				handler(w, r, p)
-			})
-
-			// 启动Web服务监听指定端口
-			err := http.ListenAndServe(":"+p, mux)
-			if err != nil {
-				fmt.Printf("启动Web服务失败：%v\n", err)
-			}
-		}(port)
+	// 使用切片去除第一个字符
+	if len(path) > 1 {
+		path = path[1:]
+	} else {
+		path = ""
 	}
 
-	// 阻止主函数退出，以保持服务一直运行
-	select {}
+	fmt.Fprint(w, path)
 }
